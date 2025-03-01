3f9118eb9f0261966f1c5bb4775fbbd6c4a672d8
İsmail Özsaygı <devozsaygi@gmail.com>
2025-03-01 16:25:46 +0300

    Added dummy SDL code for testing linking

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
+#include <SDL3/SDL.h>
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("Couldn't initialize SDL, the reason was: %s", SDL_GetError());
+        return -1;
+    }
+
+    SDL_Quit();
     return 0;
 }
