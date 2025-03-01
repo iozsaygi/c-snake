92c1d6e3322adfccb681cb16ecb8ea96e89cc70b
İsmail Özsaygı <devozsaygi@gmail.com>
2025-03-01 16:35:51 +0300

    Cleaned up main

--- src/main.c
+++ src/main.c
@@ -1,12 +1,2 @@
-#include <SDL3/SDL.h>
-#include <stdio.h>
-
 int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        printf("Couldn't initialize SDL, the reason was: %s", SDL_GetError());
-        return -1;
-    }
-
-    SDL_Quit();
-    return 0;
 }
